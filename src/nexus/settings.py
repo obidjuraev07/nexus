550dc863a15a8cb916219346ad66262a73212026
Obid Juraev <juraev@Obids-MacBook-Pro.local>
2025-03-07 17:04:47 +0500

    models updated

--- src/nexus/settings.py
+++ src/nexus/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'category'
+    'category',
+    'user',
+    'product'
 ]
 
 MIDDLEWARE = [
---
+++ src/user/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    firstname = models.CharField(max_length=30, null=False, blank=False)
+    last_name = models.CharField(max_length=30, null=True, blank=True)
+    phone_number = models.CharField(max_length=13, null=False, blank=False)
+    image = models.ImageField(upload_to='', null=True, blank=True)
+
+    def __str__(self):
+        return self.user
